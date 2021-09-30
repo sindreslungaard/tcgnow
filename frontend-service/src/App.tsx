082c769c39eec753d1348c9cffa76d6b6ce04ce1
Sindre Slungaard <slungaards@gmail.com>
2021-09-30 02:21:08 +0200

    prettier formatter

--- frontend-service/src/App.tsx
+++ frontend-service/src/App.tsx
@@ -1,11 +1,19 @@
-import IndexPage from "./pages/Index"
+import IndexPage from './pages/Index';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 const App = () => {
-  return (
-    <div>
-      <IndexPage />
-    </div>
-  )
-}
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <IndexPage />
+                </Route>
+                <Route exact path="/about">
+                    About
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
+};
 
-export default App
+export default App;
