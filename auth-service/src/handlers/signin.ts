e5f973029d2f60e2582bb4901963e83704a00afe
Sindre Slungaard <slungaards@gmail.com>
2021-10-03 03:24:13 +0200

    auth-service signin

--- auth-service/src/handlers/signin.ts
+++ auth-service/src/handlers/signin.ts
@@ -1,5 +1,43 @@
 import { Request, Response } from 'express';
+import UserModel from '../models/user';
+import bcrypt from 'bcryptjs';
 
-const signin = async (req: Request, res: Response) => {};
+const signin = async (req: Request, res: Response) => {
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).json({
+            code: 400,
+            error: 'Username and password required',
+        });
+    }
+
+    let user = await UserModel.findOne({
+        uid: req.body.username.toLowerCase(),
+    });
+
+    if (!user) {
+        return res.status(404).json({
+            code: 401,
+            error: 'User does not exist',
+        });
+    }
+
+    const ok = bcrypt.compareSync(req.body.password, user.pw_hash);
+
+    if (!ok) {
+        return res.status(400).json({
+            code: 400,
+            error: 'Incorrect username or password',
+        });
+    }
+
+    if (!user.activated) {
+        return res.status(403).json({
+            code: 403,
+            error: 'You need to activate your account before signing in',
+        });
+    }
+
+    // everything ok, sign in
+};
 
 export { signin };
